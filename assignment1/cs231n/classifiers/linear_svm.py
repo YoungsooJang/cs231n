d55e9c496420fe197bf8a5c0afff7c7ebe747d30
Youngsoo Jang <ylouang@gmail.com>
2023-08-12 18:34:21 +0900

    a1. svm - complete

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,13 +35,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i]
+        dW[:, y[i]] += -X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+  loss += reg * np.sum(W ** 2)
+  dW += 2 * reg * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -70,7 +74,16 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  # compute the loss and the gradient
+  num_train = X.shape[0]
+  scores = X @ W # (N, C)
+  correct_class_score = scores[range(num_train), y] # (N, )
+  margin = scores - correct_class_score[:, np.newaxis] + 1 # (N, C)
+  margin_max_with_zero = np.maximum(margin, 0)
+  margin_max_with_zero[range(num_train), y] = 0
+
+  loss = np.sum(margin_max_with_zero) / num_train
+  loss += reg * np.sum(W ** 2)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -85,7 +98,20 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  mask = margin_max_with_zero > 0 # (N, C)
+  dW += X.T @ mask # (D, C)
+  # X shape: (N, D)
+  coefficient = np.sum(mask, axis=1) # (N, )
+  mul = -X * coefficient[:, np.newaxis] # (N, D)
+  
+  num_classes = W.shape[1]
+  y_mask = np.zeros((num_train, num_classes))
+  y_mask[range(num_train), y] = 1
+
+  dW += mul.T @ y_mask # (D, C)
+
+  dW /= num_train
+  dW += 2 * reg * W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
